c13cfd9f9c20118eca54f0c3d3df6034386a239f
sdxmhs <huchd0@gmail.com>
2026-07-01 10:13:29 +0800

    Update copyright information in netwiz.js

--- luci-app-netwiz/htdocs/luci-static/resources/view/netwiz.js
+++ luci-app-netwiz/htdocs/luci-static/resources/view/netwiz.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 sdxmhs <https://github.com/sdxmhs/luci-app-netwiz>
+ * Copyright (C) 2026 huchd0 <https://github.com/huchd0/luci-app-netwiz>
  * Licensed under the GNU General Public License v3.0
  */
 'use strict';
